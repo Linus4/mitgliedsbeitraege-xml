ab0a03458cde87ff8858efb1e9f8290adbee1923
Linus Weber <linus.weber@protonmail.com>
2025-08-21 14:53:31 +0200

    third commit

--- README.md
+++ README.md
@@ -3,33 +3,71 @@ Erstellt eine XML Datei zum Einzug von Vereinsmitgliedsbeiträgen auf Grundlage 
 
 ## Installation
 
+Zunächst muss Python installiert werden - mindestens Version 3.11.
+
 ```
-python3 -m venv venv
+git clone https://github.com/Linus4/mitgliedsbeitraege-xml.git
+cd mitgliedsbeitraege-xml
+python3 -m venv venv # oder python
 source venv/bin/activate
 pip install -r requirements.txt
 ```
 
 ## Konfiguration
 
-Kopiere `config.toml.example` und nenne die Datei `config.toml`. Passe die Werte darin an.
+Kopiere `config.toml.example` und nenne die Datei `config.toml`. Passe die Werte 
+darin für deinen Verein passend an.
 
-## Ausführen
+## Eingabetabelle
 
 Die .ods oder .xlsx Datei, die dem Program als Eingabe gegeben wird, muss ein
-Tabellenblat mit dem Namen "Mitglieder" enthalten.
+Tabellenblat mit dem Namen `Mitglieder` enthalten.
 
-Eine Spalte "Aktiv" muss vorhanden sein. Leere Zellen werden ignoriert (inaktives
-Mitglied) und Zellen mit beliebigem Inhalt werden verarbeitet (aktives Mitglied).
+Eine Spalte `Aktiv` muss vorhanden sein. Leere Zellen werden ignoriert (inaktives
+Mitglied) und Zellen mit beliebigem Inhalt werden weiterverarbeitet (aktives Mitglied).
 
-Eine Spalte "Datum SEPA Mandat" mit einem Datum im ISO Format (z.B. "2025-08-21) 
-muss vorhanden sein.
+Eine Spalte `Datum SEPA Mandat` mit einem Datum im ISO Format (z.B. `2025-08-21`) 
+muss vorhanden sein und nicht leer sein.
 
-Die Spalten "IBAN", "BLZ", "Kontoinhaber" müssen existieren und nicht leer sein.
+Die Spalten `IBAN`, `BIC` und `Kontoinhaber` müssen existieren und nicht leer sein.
 
-Eine Spalte "Mandatsreferenz" muss existieren und nicht leer sein. Dies kann ein
+Eine Spalte `Mandatsreferenz` muss existieren und nicht leer sein. Dies kann ein
 bis zu 35 Zeichen langer String sein, der aus Buchstaben, Zahlen und Bindestrichen
 besteht und pro SEPA Lastschriftmandat vom Verein festgelegt wird. Besser nochmal 
 nachlesen.
 
-Eine Spalte "Beitrag" muss existieren und einen monatlichen Beitrag in **Cent** 
+Eine Spalte `Beitrag` muss existieren und einen monatlichen Beitrag in **Cent** 
 (ganze Zahl) enthalten.
+
+## Ausführen
+
+Vor dem Ausführen muss die virtuelle Umgebung mit `source venv/bin/activate` 
+aktiviert werden. Vor dem Prompt sollte in Klammern der Name der Umgebung 
+erscheinen.
+
+```
+usage: Mitgliedsbeiträge XML [-h] [-c CONFIGFILE] [-o OUTPUT] [-p | --print | --no-print] filename
+
+Erstellt eine XML Datei zum Einzug von Mitgliedsbeiträgen aus einer Tabelle der Vereinsmitglieder.
+
+positional arguments:
+  filename              Mitgliedertabelle als .ods oder .xlsx
+
+options:
+  -h, --help            show this help message and exit
+  -c CONFIGFILE, --configfile CONFIGFILE
+                        Konfigurationsdatei im TOML Format
+  -o OUTPUT, --output OUTPUT
+                        Ausgabe XML Datei für Sammelauftrag
+  -p, --print, --no-print
+                        Flag gibt an dass das XML auch auf dem Terminal ausgegeben werden soll.
+```
+
+### Beispiel
+
+```
+python mitgliedsbeitraege-xml.py mitgliedertabelle.ods
+```
+
+Nimmt die Konfigurationsdatei `config.toml` und schreibt in die XML Datei
+`sammelauftrag-{aktuelles-datum}.xml`.
--- mitgliedsbeitraege-xml.py
+++ mitgliedsbeitraege-xml.py
@@ -7,24 +7,47 @@ import argparse
 import sys
 from pathlib import Path
 
-def validate_member(member):
-    required_columns = ["Datum SEPA Mandat", "IBAN", "BIC", "Kontoinhaber",
+MONTHS = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", 
+          "August", "September", "Oktober", "November", "Dezember"]
+
+def validateMember(member):
+    requiredColumns = ["Datum SEPA Mandat", "IBAN", "BIC", "Kontoinhaber",
                         "Beitrag", "Mandatsreferenz"]
-    for c in required_columns:
+    for c in requiredColumns:
         if pd.isnull(member[c]):
             print(f"CRITICAL: {c} ist leer bei {member['Vorname']} {member['Nachname']}!")
             sys.exit(1)
 
+def determineCollectionDate():
+    collectionDate = (datetime.date.today().replace(day=1) + datetime.timedelta(days=32)).replace(day=1) # first of next month
+    if collectionDate - datetime.date.today() < datetime.timedelta(days=10):
+        collectionDate = datetime.date.today() + datetime.timedelte(days=10)
+        
+    return collectionDate
+
+def validateInput(members):
+    requiredColumns = set(["Nachname", "Vorname", "Aktiv", "Datum SEPA Mandat", 
+                            "IBAN", "BIC", "Kontoinhaber", "Beitrag", "Mandatsreferenz"])
+    if not requiredColumns.issubset(members.columns):
+        print(f"CRITICAL: Der Mitgliedertabelle fehlen die Spalten {requiredColumns-set(members.columns)}!")
+        sys.exit(1)
+    if not members.dtypes["Beitrag"] == "int64":
+        print(f"CRITICAL: Die Spalte 'Beitrag' muss ganze Zahlen enthalten (Centbeträge).")
+        sys.exit(1)
+
 
 if __name__ == '__main__':
 
+    todayStr = datetime.date.today().strftime('%Y-%m-%d')
+
     parser = argparse.ArgumentParser(
         prog='Mitgliedsbeiträge XML',
-        description='Erstellt eine XML Datei zum Einzug von Vereinsmitgliedsbeiträgen auf Grundlage einer Mitgliederliste.',)
+        description='Erstellt eine XML Datei zum Einzug von Mitgliedsbeiträgen aus einer Tabelle der Vereinsmitglieder.',)
 
-    parser.add_argument('filename', default="mitgliederliste.ods", help="Mitgliederliste als .ods oder .xlsx")
+    parser.add_argument('filename', default="mitgliedertabelle.ods", help="Mitgliedertabelle als .ods oder .xlsx")
     parser.add_argument('-c', '--configfile', default="config.toml", help="Konfigurationsdatei im TOML Format")
-    parser.add_argument('-o', '--output', default="sammelauftrag.xml", help="Ausgabe XML Datei für Sammelauftrag")
+    parser.add_argument('-o', '--output', default=f"sammelauftrag-{todayStr}.xml", help="Ausgabe XML Datei für Sammelauftrag")
+    parser.add_argument('-p', '--print', action=argparse.BooleanOptionalAction, help="Flag gibt an dass das XML auch auf dem Terminal ausgegeben werden soll.")
 
     args = parser.parse_args()
 
@@ -36,12 +59,9 @@ if __name__ == '__main__':
         print(f"CRITICAL: Konfigurationsdatei {args.configfile} existiert nicht!")
         sys.exit(1)
 
-    months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", 
-              "August", "September", "Oktober", "November", "Dezember"]
-
     with open(args.configfile, "rb") as f:
         config_data = tomllib.load(f)
-    print(config_data)
+    print(f"Benutze Konfiguration: {config_data}.\n")
 
     config = {
         "name": config_data["verein_name"],
@@ -56,44 +76,49 @@ if __name__ == '__main__':
     sepa = SepaDD(config, schema="pain.008.001.02", clean=True)
 
     members = pd.read_excel(args.filename, sheet_name="Mitglieder")
+    validateInput(members)
+
     members = members[~members["Aktiv"].isnull()]
     members["Datum SEPA Mandat"] = members["Datum SEPA Mandat"].dt.date
 
     print(f"Aktive Mitglieder: {len(members)}")
 
-    collection_date = (datetime.date.today().replace(day=1) + datetime.timedelta(days=32)).replace(day=1) # first of next month
-    if collection_date - datetime.date.today() < datetime.timedelta(days=10):
-        collection_date = datetime.date.today() + datetime.timedelte(days=10)
-
-    total_amount = 0
+    collectionDate = determineCollectionDate()
 
     for i, m in members.iterrows():
 
-        validate_member(m)
+        validateMember(m)
 
         payment = {
             "name": m["Kontoinhaber"],
             "IBAN": m["IBAN"],
             "BIC": m["BIC"],
             "amount": m["Beitrag"], # in cents
-            "type": "RCUR", # FRST, RCUR, OOFF, FNAL
-            "collection_date": collection_date,
+            "type": "RCUR", # FRST, RCUR, OOFF, FNAL, TODO notwendig?
+            "collection_date": collectionDate,
             "mandate_id": m["Mandatsreferenz"],
             "mandate_date": m["Datum SEPA Mandat"],
-            "description": f"{config_data['sepa_description']} {months[collection_date.month-1]} {collection_date.year}",
+            "description": f"{config_data['sepa_description']} {MONTHS[collectionDate.month-1]} {collectionDate.year}",
         }
 
         sepa.add_payment(payment)
-        total_amount += m["Beitrag"]
 
     try:
         xml = sepa.export(validate=True, pretty_print=True).decode('utf-8')
-        print(xml)
-        with open(args.output, "w", encoding='utf-8') as f:
-            f.write(xml)
     except ValidationError as e:
         print(e.__cause__)
         sys.exit(1)
+    except TypeError as e:
+        print(e)
+        sys.exit(1)
 
-    print(f"Gesamtbetrag: {total_amount/100:.2f} €")
-# TODO  AmdmntInd false?
+    print(f"Gesamtbetrag: {list(sepa._batch_totals.items())[0][1]/100:.2f} €")
+
+    with open(args.output, "w", encoding='utf-8') as f:
+        f.write(xml)
+    print(f"XML Ausgabe gespeichert in {args.output}.")
+
+    if args.print:
+        print('')
+        print(xml)
+
